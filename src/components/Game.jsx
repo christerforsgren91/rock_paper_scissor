77140ffe38d678d6a73428807702d2ba98e9f8a8
Christer <christerforsgren91@gmail.com>
2021-04-30 00:26:07 +0200

    Add image Updated README file Add styling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -25,42 +25,54 @@ const Game = () => {
   };
   return (
     <Container text>
+      <Segment padded='very'>
       <Header data-cy="header" textAlign="center" size="huge">
         Rock Paper Scissor
       </Header>
       <Header data-cy="lucky-text" textAlign="center" as="h3">
         Is it your lucky day?
       </Header>
-
-      <Grid columns="3" textAlign="center">
-        <Grid.Row>
-          <Button data-cy="rock-button" onClick={() => gameHandler("ROCK")}>
-            ROCK
-          </Button>
-          <Button data-cy="paper-button" onClick={() => gameHandler("PAPER")}>
-            PAPER
-          </Button>
-          <Button data-cy="scissor-button"onClick={() => gameHandler("SCISSOR")}>
-            SCISSOR
-          </Button>
-        </Grid.Row>
+      <Container>
+        <Grid columns="3" textAlign="center" inverted>
+          <Grid.Row>
+            <Button data-cy="rock-button" onClick={() => gameHandler("ROCK")}>
+              ROCK
+            </Button>
+            <Button data-cy="paper-button" onClick={() => gameHandler("PAPER")}>
+              PAPER
+            </Button>
+            <Button
+              data-cy="scissor-button"
+              onClick={() => gameHandler("SCISSOR")}
+            >
+              SCISSOR
+            </Button>
+          </Grid.Row>
+        </Grid>
+      </Container>
+      <Grid columns="2" textAlign="center">
+        <Segment.Group compact horizontal>
+          <Segment compact textAlign data-cy="player-score">
+            Player score:{player}
+          </Segment>
+          <Segment compact textAlign data-cy="computer-score">
+            Computer score:{computer}
+          </Segment>
+        </Segment.Group>
       </Grid>
 
-      <Segment.Group horizontal>
-        <Segment compact textAlign data-cy="player-score">
-          Player score:{player}
-        </Segment>
-        <Segment compact textAlign data-cy="computer-score">
-          Computer score:{computer}
-        </Segment>
-      </Segment.Group>
-
-      <Button data-cy="restart-button" onClick={() => restartGame("Restart")}>
-        Restart
-      </Button>
-
-      <p data-cy="tie" >{tie}</p>
+      <Grid textAlign="center">
+        <Button data-cy="restart-button" onClick={() => restartGame("Restart")}>
+          Restart
+        </Button>
+      </Grid>
+      </Segment>
+      <Grid textAlign="center">
+        <p data-cy="tie">{tie}</p>
+      </Grid>
+      
     </Container>
+    
   );
 };
 
